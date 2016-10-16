14e79b43baba857b22ea317dea65b6ffa7706979
Joe Rodrigues <joseph_rodrigues44@yahoo.com>
2016-10-16 14:27:57 -0300

    First Commit Using Git Desktop

---
+++ Homework3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float SecInMin, SecInDay, SecInHour;
+            float NumOfDays, NumOfHours, NumOfMin, NumOfSec;
+
+            //Get user input   
+            Console.Write("Please enter the number of seconds: ");
+            string sec = Console.ReadLine();
+            float x = float.Parse(sec);
+
+            //Initialize number of seconds in a minute
+            SecInMin = 60;
+
+            //Initialize number of seconds in an hour
+            SecInHour = SecInMin * SecInMin;
+
+            //Initialize number of seconds in a day
+            SecInDay = SecInHour * 24;
+
+            //Calculate number of days, hours, minutes and seconds from the user input
+            NumOfDays = (x / SecInDay);
+            NumOfHours = (x / SecInHour);
+            NumOfMin = (x / SecInMin);
+            NumOfSec = (NumOfMin * SecInMin);
+
+            Console.WriteLine("The calculated conversions are {0} days, {1} hours, {2} minutes & {3} seconds.", NumOfDays, NumOfHours, NumOfMin, NumOfSec);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Session3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session3
+{
+    class Program
+    {
+        static void Main(string[] arg)
+        {
+            Console.Write("Please enter your input: ");
+            string str = Console.ReadLine();
+            int x = int.Parse(str);
+            int Result = (int)(5 *x*x + 6);
+            Console.WriteLine("The calculated value is {0}", Result);
+            Console.ReadLine();          
+        }
+    }
+}
